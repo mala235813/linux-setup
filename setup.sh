921b76d93a936d91b9a7dd1f56dc94cb8c824156
Mårten Larsson <mala235813@gmail.com>
2019-09-25 21:38:30 +0200

    Ssh key gen support

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Read email
+read -p "Enter your email: " email
+
+echo Email is ${email}
+
+ssh-keygen -t rsa -b 4096 -C "${email}"
+
+echo "Public keys:"
+find ~/.ssh -name '*.pub' | xargs cat
+echo -e "Go to \e[4mhttps://github.com\e[0m and add a public key"
+echo -e "Go to \e[4mhttps://gitlab.com\e[0m and add a public key"
+
+read -p "Press enter to continue"
+
+
 # Uninstall arch packages
 yay -Rc --noconfirm - < ./package-lists/repo-remove
 

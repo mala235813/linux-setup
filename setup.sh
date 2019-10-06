ba9124fe7c62431b56d9beb6aa9437b2b5811ff0
Mårten Larsson <mala235813@gmail.com>
2019-10-06 10:44:52 +0200

    Cleanup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+# Uninstall packages
+yay -Rc --noconfirm chromium
 
-# Read email
+# Install packages
+yay -S --noconfirm firefox
+yay -S --noconfirm kdiff3-qt
+yay -S --noconfirm xclip
+yay -S --noconfirm clojure
+
+
+# Setup Spacemacs
+yay -S --noconfirm emacs
+yay -S --noconfirm ttf-unifont
+rm -rf ~/.emacs.d
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+# Setup ssh keys
 read -p "Enter your email: " email
 
 echo Email is ${email}
@@ -11,23 +26,4 @@ echo "Public keys:"
 find ~/.ssh -name '*.pub' | xargs cat
 echo -e "Go to \e[4mhttps://github.com\e[0m and add a public key"
 echo -e "Go to \e[4mhttps://gitlab.com\e[0m and add a public key"
-
-read -p "Press enter to continue"
-
-
-# Uninstall arch packages
-yay -Rc --noconfirm - < ./package-lists/repo-remove
-
-# Uninstall aur packages
-yay -Rc --noconfirm - < ./package-lists/aur-remove
-
-# Install arch packages
-yay -S --noconfirm - < ./package-lists/repo-add
-
-# Install aur packages
-yay -S --noconfirm - < ./package-lists/aur-add
-
-# Setup Spacemacs
-rm -rf ~/.emacs.d
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
